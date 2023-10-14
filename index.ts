5eadceae6bd030f09992395f70b50724b66dcd75
hori <fourbasic19@gmail.com>
2023-10-14 23:38:18 +0900

    vercel更新

--- index.ts
+++ index.ts
@@ -11,9 +11,7 @@ app.use(cors());
 const prisma = new PrismaClient();
 
 app.get("/api/v1/users", async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany({
-    include: { Posts: true },
-  });
+  const users = await prisma.user.findMany();
   return res.json(users);
 });
 
@@ -32,72 +30,5 @@ app.post("/api/v1/users", async (req: Request, res: Response) => {
   }
 });
 
-app.put("/api/v1/users/:id", async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  const { name } = req.body;
-  try {
-    const user = await prisma.user.update({
-      where: {
-        id,
-      },
-      data: {
-        name,
-      },
-    });
-    return res.json(user);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
-});
-
-app.delete("/api/v1/users/:id", async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-
-  try {
-    const user = await prisma.user.delete({
-      where: {
-        id,
-      },
-    });
-    return res.json(user);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
-});
-
-app.get("/api/v1/users/:id", async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        id,
-      },
-    });
-    return res.json(user);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
-});
-
-app.post("/api/v1/posts", async (req: Request, res: Response) => {
-  const { title, content, authorId } = req.body;
-  try {
-    const post = await prisma.post.create({
-      data: {
-        title,
-        content,
-        authorId,
-      },
-    });
-    return res.json(post);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
-});
-
-app.get("/api/v1/posts", async (req: Request, res: Response) => {
-  const posts = await prisma.post.findMany();
-  return res.json(posts);
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
