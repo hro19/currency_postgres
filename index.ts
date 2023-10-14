725270224931b798f056e061545425d4178a940b
hori <fourbasic19@gmail.com>
2023-10-14 16:15:17 +0900

    vercel更新

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const port = 5000;
@@ -8,7 +7,6 @@ app.use(express.json());
 const cors = require('cors');
 app.use(cors());
 
-const prisma = new PrismaClient();
 
 app.get("/", (req: Request, res: Response) => res.json({ "mes": "Hello World!" }));
 
